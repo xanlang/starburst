985ec4cec1e26d0bd1ba336f2f07caf1e9b127c1
xanlang <xanlanga@gmail.com>
2017-03-26 21:34:18 -0700

    New Snippet TextChat
    First Attempt to sync visual studio windows with linux

---
+++ Snippets/170326-TextChat/TextChat/TextChat/TextChat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace TextChat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+            GetLine();
+        }
+
+        static void GetLine()
+        {
+            System.Net.WebClient wc = new System.Net.WebClient();
+            string result = wc.DownloadString("http://www.capsule03.com/wiki.html");
+            Console.WriteLine(result);
+        }
+    }
+
+}
