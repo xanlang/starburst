70ba01b9ac99fb655f2c3ec88dc735103d01f7a3
Xan Phone <xanlanga@gmail.com>
2019-01-25 03:43:05 -0800

    up to tutorial 7

--- Django/tutsite/polls/views.py
+++ Django/tutsite/polls/views.py
@@ -3,29 +3,27 @@ from django.shortcuts import get_object_or_404
 from django.http import Http404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question
 from .models import Choice
 
 # Create your views here.
-def index(request):
-  latest_question_list = Question.objects.order_by('-pub_date')[:5]
-  template = loader.get_template('polls/index.html')
-  context = { 'latest_question_list':
-    latest_question_list,
-    }
-  return HttpResponse(template.render(context, request))
-def detail(request, question_id):
-  question = get_object_or_404(Question, pk=question_id)
-  return render(request, 'polls/detail.html', {'question': question})
 
+class IndexView(generic.ListView):
+  template_name = 'polls/index.html'
+  context_object_name = 'latest_question_list'
+  def get_queryset(self):
+    """ Return the last five published questions"""
+    return Question.objects.order_by('-pub_date')[:5]
+ 
+class DetailView(generic.DetailView):
+  model = Question
+  template_name = 'polls/detail.html'
 
-
-def results(request, question_id):
-  question = get_object_or_404(Question, pk=question_id)
-  return render(request, 'polls/results.html', {'question': question})
-
-
+class ResultsView(generic.DetailView):
+  model = Question
+  template_name = 'polls/results.html'
 
 def vote(request, question_id):
   question = get_object_or_404(Question, pk=question_id)
