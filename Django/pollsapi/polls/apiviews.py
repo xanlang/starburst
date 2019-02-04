ce32ec459a75a4b4ea64a9e4175c041a75973dee
Xander Lang <xanlanga@gmail.com>
2019-02-03 18:44:52 -0800

    login stuff

--- Django/pollsapi/polls/apiviews.py
+++ Django/pollsapi/polls/apiviews.py
@@ -7,6 +7,8 @@ from rest_framework import viewsets
 from .models import Poll, Choice
 from .serializers import PollSerializer, VoteSerializer, ChoiceSerializer, UserSerializer
 
+from django.contrib.auth import authenticate
+
 class PollViewSet(viewsets.ModelViewSet):
     queryset = Poll.objects.all()
     serializer_class = PollSerializer
@@ -40,3 +42,15 @@ class UserCreate(generics.CreateAPIView):
     authentication_classes = ()
     permission_classes = ()
     serializer_class = UserSerializer
+
+class LoginView(APIView):
+    permission_classes = ()
+
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        user = authenticate(username = username, password = password)
+        if user:
+            return Response({"token":user.auth_token.key})
+        else:
+            return Response({"error": "Wrong Credentials"}, status=status.HTTP_400_BAD_REQUEST)
